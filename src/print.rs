bcf2466964227849e0c2de8764ee5af6ce2ed986
sidewinder040 <macevic040@hotmail.co.uk>
2022-12-02 14:54:46 +0000

    Printing

---
+++ src/print.rs
@@ -0,0 +1,10 @@
+pub fn run() {
+    // Print to console
+    println!("Hello from the print.rs file");
+
+    println!("Number: {}", 1);
+
+    println!("My name is {} and I live in {}", "Fred", "England");
+
+    println!("I am {0} and {0} likes to code in {1}.", "Mark", "Rust");
+}
